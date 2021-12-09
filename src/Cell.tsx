e9ed6df9bf2a037818a64d939088908b58e9e11d
axiom777 <axiom777@yandex.ru>
2021-12-09 15:58:42 +0300

    Add data attrs and title for hover

--- src/Cell.tsx
+++ src/Cell.tsx
@@ -4,18 +4,29 @@ import { config, Hour } from "./interfaces";
 type CellProps = Hour;
 class Cell extends React.Component<CellProps> {
   render() {
-
     // eslint-disable-next-line
-    const { timeForm, timeTo,calendrEvents } = this.props;
-    const fromStr = timeForm.toLocaleString("RU-ru",{hour:"2-digit", minute:"2-digit"})
-    const toStr = timeTo.toLocaleString("RU-ru",{hour:"2-digit", minute:"2-digit"})
+    const { timeForm, timeTo, calendrEvents } = this.props;
+    const fromStr = timeForm.toLocaleString("RU-ru", {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+    const toStr = timeTo.toLocaleString("RU-ru", {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
     const hoursString = `${fromStr} - ${toStr}`;
+
+    const attrs = {
+      "data-from": new Date(timeForm).toISOString(),
+      "data-to": new Date(timeTo).toISOString(),
+    };
 
     return (
       <div
         style={{ height: `${config.cellHeightPx}px` }}
         className="time"
         title={hoursString}
+        {...attrs}
       ></div>
     );
   }
