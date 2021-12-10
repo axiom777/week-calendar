6704b7478bc61d10c5a208bc061ac1dcbed5528c
axiom777 <axiom777@yandex.ru>
2021-12-10 16:23:22 +0300

    Fix grouping douplicate

--- src/App.tsx
+++ src/App.tsx
@@ -52,17 +52,20 @@ export default class App extends React.Component<{}, AppState> {
     // searching for longest not intersected continues periods
     for (let event of events) {
       const { from, to } = event;
-      if (lnip.length === 0) lnip.push({ dates: [from, to], events: [event] });
-      const currentLnip = lnip[lnip.length - 1];
-      const toLnip = currentLnip.dates[1];
+      const isFirst = lnip.length === 0;
+      if (isFirst) lnip.push({ dates: [from, to], events: [event] });
+      if (!isFirst) {
+        const currentLnip = lnip[lnip.length - 1];
+        const toLnip = currentLnip.dates[1];
 
-      if (from.getTime() < toLnip.getTime()) {
-        currentLnip.dates[1] = new Date(
-          Math.max(toLnip.getTime(), to.getTime())
-        );
-        currentLnip.events.push(event);
-      } else {
-        lnip.push({ dates: [from, to], events: [event] });
+        if (from.getTime() < toLnip.getTime()) {
+          currentLnip.dates[1] = new Date(
+            Math.max(toLnip.getTime(), to.getTime())
+          );
+          currentLnip.events.push(event);
+        } else {
+          lnip.push({ dates: [from, to], events: [event] });
+        }
       }
     }
 
